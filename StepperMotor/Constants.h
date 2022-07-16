11ff0e0a9ee57ecd8b42c53c5bd2c7ed3ca1a86d
ArwaShamardal <76781020+ArwaShamardal@users.noreply.github.com>
2022-07-16 17:27:55 +0200

    Added Functions/Cooking durations
    -Changed delay to myDelay using millis()
    -Changed main loop to call a mixer function

--- StepperMotor/Constants.h
+++ StepperMotor/Constants.h
@@ -5,26 +5,22 @@ enum switcher {
   ON, // enable
   OFF // disable
 };
+
 enum pullDirection {
   UP,  
   DOWN,
   UNDECLARED 
 };
+
 const uint8_t mixerMotorPin1 = 2;
 const uint8_t mixerMotorPin2 = 3;
 const uint8_t pulleyMotorPin1 = 4;
 const uint8_t pulleyMotorPin2 = 5;
+
+const unsigned long jellyMixingDuration = 8000UL;
+const unsigned long cakeMixingDuration = 15000UL;
+unsigned long previousMillis = 0UL;
+
 enum switcher mixerSystemSwitch = ON;
 
 #endif
-
-
-/*        Motor modes
- * AccelStepper::FULL2WIRE (2) means: 2 wire stepper (2 pins needed). 
- * AccelStepper::FULL3WIRE (3) means: 3 wire stepper, like a harddisk motor (3 pins needed). 
- * AccelStepper::FULL4WIRE (4) means: 4 wire stepper (4 pins needed). 
- * AccelStepper::HALF3WIRE (6) means: 3 wire half stepper, like a harddisk motor (3 pins needed) 
- * AccelStepper::HALF4WIRE (8) means: 4 wire half stepper (4 pins needed) 
- *
- * AccelStepper uses AccelStepper::FULL4WIRE (4 pins needed) by default.
- */
--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,6 +1,3 @@
-// Assume that pulling up/down the mixer takes 5 seconds
-// Assume that any recipe takes 3 seconds to be mixed
-
 #include "Constants.h"
 
 void setup() {
@@ -13,38 +10,40 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:   
+  unsigned long foodDuration = cakeMixingDuration; // To be changed accordingly
+  foodMixer(foodDuration);
+  myDelay(1000UL);             // to be removed
+}
 
-if(mixerSystemSwitch == ON){
+
+void foodMixer(unsigned long cookingTime){
+  if(mixerSystemSwitch == ON){
   // This part os for pulling down the mixer
   Serial.println("Pulling down");
   motorEnabler(pulleyMotorPin1,pulleyMotorPin2, ON, DOWN);
-  delay(5000); // to be changed
+  myDelay(1000UL); // to be changed according to the speed
 
 
   //This part is for mixing the ingredients
   Serial.println("Mixing");
   motorEnabler(pulleyMotorPin1,pulleyMotorPin2, OFF, UNDECLARED);
   motorEnabler(mixerMotorPin1,mixerMotorPin2, ON, UNDECLARED);
-  delay(3000); // to be changed
+  myDelay(cookingTime); // determined according to the time needed for the food selected
 
 
   //This part is for pulling up the mixer  
   Serial.println("Pulling up");
   motorEnabler(mixerMotorPin1,mixerMotorPin2, OFF, UNDECLARED);
   motorEnabler(pulleyMotorPin1,pulleyMotorPin2, ON, UP);
-  delay(5000); // to be changed
+  myDelay(1200UL); // to be changed according to the speed
 
   //System shutting down
   Serial.println("system turining off");
   motorEnabler(pulleyMotorPin1,pulleyMotorPin2, OFF, UNDECLARED);
   mixerSystemSwitch = OFF;
-
+  }
 }
 
-  delay(1000);             // to be removed
-  Serial.println("Done");  // to be removed
-}
 
 void motorEnabler(uint8_t motorPin1,uint8_t motorPin2, enum switcher switcher, enum pullDirection pullDirection){
   if(switcher == ON){
@@ -60,5 +59,20 @@ void motorEnabler(uint8_t motorPin1,uint8_t motorPin2, enum switcher switcher, e
   else if (switcher = OFF){
     digitalWrite(motorPin1, LOW);
     digitalWrite(motorPin2, LOW);
+  }
+}
+
+
+void myDelay(unsigned long milliSeconds){
+  previousMillis = millis();
+  bool finished = false;
+  unsigned long currentMillis;
+  while (!finished){
+    currentMillis = millis();
+    
+    if(currentMillis - previousMillis > milliSeconds)
+    {
+      finished = true;
+    }  
   }
 }
